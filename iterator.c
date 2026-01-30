dfa92bc7cd68cca0a3457209acac5db0b0c9b32b
Gabriel TESSIER <gabriel.tessier45@gmail.com>
2026-01-30 11:40:45 +0100

    add poll to store unsed stack

--- iterator.c
+++ iterator.c
@@ -1,10 +1,36 @@
 
 #include <unistd.h>
 #include <sys/mman.h>
+#include <stdlib.h>
+#include <assert.h>
 
 #include "iterator.h"
 
 #define STACK_CAPACITY (1024*getpagesize())
+
+#define DA_INIT_CAP 10
+
+typedef struct {
+  void **addrs;   // array of address (stack base)
+  size_t capacity;
+  size_t count;
+} poll;
+
+poll stack_poll = {0};
+
+void poll_append(void *addr) {
+  if (stack_poll.count >= stack_poll.capacity) {
+    stack_poll.capacity = stack_poll.capacity == 0 ? DA_INIT_CAP : stack_poll.capacity*2;
+    stack_poll.addrs = realloc(stack_poll.addrs, stack_poll.capacity*sizeof(*stack_poll.addrs));
+    assert(stack_poll.addrs != NULL && "Buy more RAM lol");
+  }
+  stack_poll.addrs[stack_poll.count++] = addr;
+}
+
+void poll_remove_unordered(size_t i) {
+  assert(i < stack_poll.count);
+  stack_poll.addrs[i] = stack_poll.addrs[--stack_poll.count];
+}
 
 // Linux x86_64 call convention
 // %rdi, %rsi, %rdx, %rcx, %r8, and %r9
@@ -30,6 +56,10 @@
   "popq %rdi")
 
 void __attribute__((naked)) iterator_end(void) {
+  // add iterator stack to stack_poll
+  asm("movq (%rsp), %rdi\n\t"
+      "movq 8(%rdi), %rdi\n\t"
+      "call poll_append");
   asm("popq %rdi\n\t"          // pop iterator struct
       "movb $1, 24(%rdi)\n\t"  // iter->finish = true
       "movq $0, %rsi\n\t"      // call iterator_yield(iter, 0);
@@ -38,7 +68,12 @@ void __attribute__((naked)) iterator_end(void) {
 }
 
 void iterator_init(Iterator *iter, void (*f)(Iterator*, void*), void *args) {
-  iter->stack_base = mmap(NULL, STACK_CAPACITY, PROT_WRITE|PROT_READ, MAP_PRIVATE|MAP_STACK|MAP_ANONYMOUS|MAP_GROWSDOWN, -1, 0);
+  if (stack_poll.count != 0) {
+    iter->stack_base = stack_poll.addrs[0];
+    poll_remove_unordered(0);
+  } else {
+    iter->stack_base = mmap(NULL, STACK_CAPACITY, PROT_WRITE|PROT_READ, MAP_PRIVATE|MAP_STACK|MAP_ANONYMOUS|MAP_GROWSDOWN, -1, 0);
+  }
   iter->finish = false;
   void **rsp = (void**)((char*)iter->stack_base + STACK_CAPACITY);
   // @arch
